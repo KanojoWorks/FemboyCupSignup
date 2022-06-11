49edbafc04e565f2585fccdeaa572d656e531c89
Shivam Ramkisoen <s.r.v.ramkisoen@umail.leidenuniv.nl>
2022-06-11 23:27:51 +0200

    Fix bws not rounding

--- server/util.ts
+++ server/util.ts
@@ -1,6 +1,6 @@
 export function calculateBws(rank: number, badges: number): number {
     const badgeResult: number = Math.pow(badges, 2)
     const x: number = Math.pow(0.9937, badgeResult)
-    const bws: number = Math.pow(rank, x)
+    const bws: number = Math.round(Math.pow(rank, x))
     return bws
 }
